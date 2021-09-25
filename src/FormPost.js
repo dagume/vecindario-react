fbbc153fcbfd1c380b4eebf541c8bf737e0b8b8a
Daniel Guecha <guechadaniel@gmail.com>
2021-09-24 21:25:05 -0500

    refactor/form post ♻️

--- src/FormPost.js
+++ src/FormPost.js
@@ -1,5 +1,5 @@
 import { Avatar, Button } from '@material-ui/core'
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import './FormPost.css'
 
 
@@ -11,15 +11,14 @@ function FormPost() {
     setData({...data, [name]:value})
   }
   function handleSubmit(){
-    console.log(data)
     fetch('https://dry-dusk-90160.herokuapp.com/api/post',{
       method:'POST',
       headers:{'Content-Type' : 'application/json'},
       body:JSON.stringify(data)
     })
     .then(response => { 
-      console.log(response)
       response.json()
+      window.location.reload();
     })
   }
 
@@ -28,11 +27,11 @@ function FormPost() {
       <form >
         <div className="form_input" >
           <Avatar />
-          <input onChange={({target})=>handleChange(target)} name="name" type="text" placeholder="Escribe el titulo"></input>
+          <input onChange={({target})=>handleChange(target)} name="name" type="text" placeholder="Escribe el titulo" ></input>
         </div>
-          <textarea onChange={({target})=>handleChange(target)} name="content" className="form_textarea" type="text" rows="5" placeholder="Escribe un comentario"></textarea>
-          <input onChange={({target})=>handleChange(target)} name="owner_email" className="form_input_mail" type="email" placeholder="Escribe el correo"></input>
-        <Button type="button" onClick={handleSubmit} className="form_button">Publicar post</Button>
+          <textarea onChange={({target})=>handleChange(target)} name="content" className="form_textarea" type="text" rows="5" placeholder="Escribe un comentario" ></textarea>
+          <input onChange={({target})=>handleChange(target)} name="owner_email" className="form_input_mail" type="email" placeholder="Escribe el correo" ></input>
+        <Button type="button" onClick={handleSubmit} className="form_button">Publicar</Button>
       </form>
     </div>
   )
